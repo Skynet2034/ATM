db10d65db663c8a3a488e548aebc94538a1aeb6f
Иван <ivan1978_04@mail.ru>
2019-11-07 21:06:02 +0300

    Rewrited benchmark

--- src/homeWorkLesson1/CollectionsTest.java
+++ src/homeWorkLesson1/CollectionsTest.java
@@ -1,72 +1,75 @@
 package homeWorkLesson1;
 
 public class CollectionsTest {
-    public static void benchmark(int length) {
-        ListArrayCollection<Integer> arr = new ListArrayCollection<>();
-        ListLinkedCollection<Integer> linked = new ListLinkedCollection<>();
-        for (int i = 0; i < length; i++) {
+    public static <E> void  benchmark (E[] testData) {
+        ListArrayCollection<E> arr = new ListArrayCollection<>();
+        ListLinkedCollection<E> linked = new ListLinkedCollection<>();
+        /*for (int i = 0; i < testData.length; i++) {
             arr.add(i);
             linked.add(i);
-        }
+        }*/
 
-        System.out.println("Array - добавление 1000 элементов в конец");
+        System.out.println("Array - добавление " +testData.length+" элементов в конец");
         long startTime = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++)
-            arr.add(i);
+        for (int i = 0; i < testData.length; i++)
+            arr.add(testData[i]);
+        System.out.println("Время - " + (System.currentTimeMillis() - startTime));
+        arr.clear();
+        System.out.println("Array - добавление " +testData.length+" элементов в начало");
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < testData.length; i++)
+            arr.addFirst(testData[i]);
         System.out.println("Время - " + (System.currentTimeMillis() - startTime));
 
-        System.out.println("Array - добавление 1000 элементов в начало");
+
+        System.out.println("LinkedList - добавление " +testData.length+" элементов в конец");
         startTime = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++)
-            arr.addFirst(i);
+        for (int i = 0; i < testData.length; i++)
+            linked.add(testData[i]);
         System.out.println("Время - " + (System.currentTimeMillis() - startTime));
 
-        System.out.println("LinkedList - добавление 1000 элементов в конец");
+int start=testData.length/5;
+        int finish=testData.length-start;
+
+        System.out.println("Array - поиск "+(finish-start)+" элементов в середине по значению (с "+start+" по "+finish+")");
         startTime = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++)
-            linked.add(i);
+        for (int i = start; i < finish; i++)
+            arr.contains(testData[i]);
         System.out.println("Время - " + (System.currentTimeMillis() - startTime));
 
-        System.out.println("LinkedList - добавление 1000 элементов в начало");
+        System.out.println("Array - поиск "+(finish-start)+" элементов в середине по индексу (с "+start+" по "+finish+")");
         startTime = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++)
-            linked.addFirst(i);
-        System.out.println("Время - " + (System.currentTimeMillis() - startTime));
-
-        System.out.println("Array - поиск 5000 элементов в середине по значению (с 1000 по 6000)");
-        startTime = System.currentTimeMillis();
-        for (int i = 1000; i < 6000; i++)
-            arr.contains(Integer.valueOf(i));
-        System.out.println("Время - " + (System.currentTimeMillis() - startTime));
-
-        System.out.println("Array - поиск 5000 элементов в середине по индексу (с 1000 по 6000)");
-        startTime = System.currentTimeMillis();
-        for (int i = 1000; i < 6000; i++)
+        for (int i = start; i < finish; i++)
             arr.get(i);
         System.out.println("Время - " + (System.currentTimeMillis() - startTime));
 
-        System.out.println("LinkedList - поиск 5000 элементов в середине (с 1000 по 6000)");
+        System.out.println("LinkedList - поиск "+(finish-start)+" элементов в середине (с "+start+" по "+finish+")");
         startTime = System.currentTimeMillis();
-        for (int i = 1000; i < 6000; i++)
-            linked.contains(i);
+        for (int i = start; i < finish; i++)
+            linked.contains(testData[i]);
         System.out.println("Время - " + (System.currentTimeMillis() - startTime));
 
-        System.out.println("Array - удаление 5000 элементов в начале");
+        System.out.println("Array - удаление "+testData.length/2+" элементов в начале");
         startTime = System.currentTimeMillis();
         Iterator it = arr.getIterator();
-        for (int i = 0; i < 5000; i++)
+        for (int i = 0; i < testData.length/2; i++)
             it.remove();
         System.out.println("Время - " + (System.currentTimeMillis() - startTime));
 
-        System.out.println("LinkedList - удаление 5000 элементов в начале");
+        System.out.println("LinkedList - удаление "+testData.length/2+"  элементов в начале");
         startTime = System.currentTimeMillis();
         Iterator it2 = linked.getIterator();
-        for (int i = 0; i < 5000; i++)
+        for (int i = 0; i < testData.length/2; i++)
             it2.remove();
         System.out.println("Время - " + (System.currentTimeMillis() - startTime));
     }
 
     public static void main(String[] args) {
-        benchmark(10000);
+        Integer[] testData=new Integer[100000];
+        for (int i=0;i<testData.length;i++)
+        {
+            testData[i]=Integer.valueOf(i);
+        }
+        benchmark(testData);
     }
 }
